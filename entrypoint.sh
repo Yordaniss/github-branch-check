acc282aacf8526e7689877e4734d6cb0585294e4
Dmytro Werner <wernerdmytro@gmail.com>
2025-08-07 22:01:37 +0200

    * added more rights

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,12 @@
 set -e
 
 TARGET_BRANCH="$1"
-BASE_BRANCH="${2:-main}"
-CREATE_ISSUE="${3:-true}"
+BASE_BRANCH="$2"
+CREATE_ISSUE="$3"
 
-echo "🚀 Starting maintenance check..."
 echo "🔁 Comparing '$TARGET_BRANCH' against '$BASE_BRANCH'"
 echo "📝 Create issue if changes found: $CREATE_ISSUE"
+
+chmod +x "${GITHUB_ACTION_PATH}/check.sh"
 
 "${GITHUB_ACTION_PATH}/check.sh" "$TARGET_BRANCH" "$BASE_BRANCH" "$CREATE_ISSUE"
